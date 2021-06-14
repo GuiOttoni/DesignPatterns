fc298ff58c1bade30719544602a9306eafbaf96c
Guilherme Ottoni Escarelli De Alencar <guiottonie@gmail.com>
2021-06-14 14:30:44 -0300

    Add singleton pattern

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DesignPatterns.Creational;
+using System;
 
 namespace DesignPatterns
 {
@@ -6,7 +7,7 @@ namespace DesignPatterns
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
