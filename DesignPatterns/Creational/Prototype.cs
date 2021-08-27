7a6f366ed236f6cc7a94dacd1cb5a045173aaca0
Guilherme Ottoni Escarelli De Alencar <guiottonie@gmail.com>
2021-08-26 21:05:24 -0300

    adiciona prototype

---
+++ DesignPatterns/Creational/Prototype.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational
+{
+    public class Zed : ICloneable
+    {
+        public Vector3 Position { get; set; }
+        public string Id { get; set; }
+        public string PlayerName { get; set; }
+
+
+        public Zed(Vector3 pos, string id, string playerName)
+        {
+            Position = pos;
+            Id = id;
+            PlayerName = playerName;
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,41 @@
+﻿using DesignPatterns.Creational;
+using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestSingleton();
+            TestPrototype();
+        }
+
+
+        private static void TestSingleton()
+        {
+            Singleton singleton = Singleton.GetInstance;
+
+            singleton.SaySomething();
+
+            Console.ReadLine();
+        }
+
+        private static void TestPrototype()
+        {
+            var original = new Zed(new System.Numerics.Vector3(10, 10, 10), "GUID-1", "PlayerName");
+            Console.WriteLine(original.Position);
+            Console.WriteLine(original.Id);
+            Console.WriteLine(original.PlayerName);
+
+            Zed clone = (Zed)original.Clone();
+            clone.Position = new System.Numerics.Vector3(20, 20, 20);
+            clone.Id = "GUID-2";
+            Console.WriteLine(clone.Position);
+            Console.WriteLine(clone.Id);
+            Console.WriteLine(clone.PlayerName);
+
+            Console.ReadLine();
+        }
+    }
+}
